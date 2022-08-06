fa2a567159087bbd45f456d0fe9b19a9e89ab81f
annuttas <108422647+annuttas@users.noreply.github.com>
2022-08-07 00:52:53 +0300

    Add files via upload

---
+++ Task_29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// Примеры:
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int N = 8;
+Console.WriteLine("Введите 8 чисел ");
+int[] myArray = new int[N];
+for (int i = 0; i < N; i++)
+    myArray[i] = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < N; i++)
+    Console.Write("{0} ", myArray[i]);
